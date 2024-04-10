2087008e4d0bdf0600e60bf3b6c9bd1018259156
Mahabub <mahabubx7@gmail.com>
2024-04-10 14:05:22 +0600

    fix(husky): fixing hooks and lint-staged

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    //   TODO Add Scope Enum Here
+    // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        // 'style',
+        'refactor',
+        'ci',
+        'test',
+        'revert',
+        'perf',
+        'init',
+        'data',
+        'config',
+        'tweak',
+      ],
+    ],
+  },
+};
