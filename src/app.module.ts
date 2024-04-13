106a6975fdb08be6b2dcf4cdf0343d7b1d1c8d5a
Mahabub <mahabubx7@gmail.com>
2024-04-13 11:54:33 +0600

    chore(env): environment & sample docker files added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { envVariables } from './config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, load: [envVariables] })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/index.ts
@@ -0,0 +1,6 @@
+import variablesConfig from './variables.config';
+
+const envVariables = variablesConfig;
+
+// Exports the config object
+export { envVariables };
